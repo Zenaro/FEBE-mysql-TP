0b524e5b406df4039edfc2a93f5adb92cfbbf01c
Zenaro <572001239@qq.com>
2017-02-28 23:32:41 +0800

    style

--- front-end/src/component/container.vue
+++ front-end/src/component/container.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
   	<div class="header">
-	    <div class="header-center">
+	    <div class="header-banner center">
 	        <h1>
 	            <router-link to="/">ZMedia音乐</router-link>
 	        </h1>
@@ -60,14 +60,17 @@ module.exports = {
 
 <style lang="sass">
 $imgUrlPrefix: 'http://localhost/NewWorld/sql-CloudMusic/FEBE-mysql/front-end/src/image/sprite/';
+/* ---- header ------- */
 .header {
 	width: 100%;
 	height: 105px;
 	min-width: 1000px;
 	background: #F2FAF7;
-	.header-center {
+	.center {
 		width: 1000px;
 		margin: 0 auto;
+	}
+	div.header-banner {
 		height: 70px;
 		> h1 {
 			width: 180px;
@@ -131,8 +134,127 @@ $imgUrlPrefix: 'http://localhost/NewWorld/sql-CloudMusic/FEBE-mysql/front-end/sr
 					background: url($imgUrlPrefix + 'sprite.png');
 					background-position: -440px -10px;
 				}
+				ul.result {
+					width: 250px;
+					background: #fff;
+					position: absolute;
+					padding: 5px 0;
+					border: solid 1px #ccc;
+					border-radius: 7px;
+					display: none;
+					top: 35px;
+					left: 0;
+					li {
+						width: 99%;
+						height: 30px;
+						line-height: 30px;
+						text-indent: 10px;
+						a {
+							overflow: hidden;
+							display: block;
+							font-size: 10px;
+							&.active, &:hover {
+								background: #f1f1f1;
+							}
+							.col {
+								float: left;
+								white-space: nowrap;
+								overflow: hidden;
+								text-overflow: ellipsis;
+								&.result-master {
+									max-width: 30%;
+									color: #0C73C2;
+								}
+								&.col-str {
+									max-width: 10%;
+								}
+								&.result-name {
+									max-width: 60%;
+								}
+							}
+						}
+					}
+				}
+			}
+			.top-user {
+				float: left;
+				color: #666;
+				cursor: pointer;
+				background: inherit;
+				line-height: 70px;
+				.user-memb{
+					height: auto;
+					line-height: 70px;
+					display: none;
+					float: left;
+					position: relative;
+					h4 {
+						font-weight: normal;
+					}
+					ul.slide-down {
+						width: 90px;
+						padding: 5px 0;
+						text-align: center;
+						background: #fafafa;
+						border: solid 1px #aaa;
+						border-radius: 4px;
+						position: absolute;
+						top: 50px;
+						left: -30px;
+						display: none;
+						li {
+							width: 88px;
+							height: 25px;
+							line-height: 25px;
+							a {
+								display: block;
+								&:hover {
+									background: #F2FAF7;
+								}
+							}
+						}
+					}
+				}
 			}
 		}
+	}
+	div.sub-list {
+		width: 100%;
+		height: 35px;
+		overflow: hidden;
+		background: #777;
+		ul.sub-nav {
+			margin-left: 190px;
+			li {
+				padding: 0 15px;
+				color: #03DEDB;
+				line-height: 35px;
+				float: left;
+				a {
+					padding: 0 12px;
+					border-radius: 10px;
+					font-size: 12px;
+					&:hover, &:focus, &.active {
+						color: #F5CE03;
+						background: #666;
+					}
+				}
+			}
+		}
+	}
+}
+/* --------- end ------------ */
+
+/* ------- 主页内容 --------- */
+.wrap{
+	width: 100%;
+	min-width: 1100px;
+	margin: 0 auto;
+	background: #fafafa;
+	.wrap-in{
+		width: 1100px;
+		margin: 0 auto;
+		background: #FAFAF5;
 	}
 }
 </style>
